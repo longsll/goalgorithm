b12f47df1d0c52d99bfbcfc2eec936d620ecb617
longsll <1506911471@qq.com>
2023-04-01 19:21:58 +0800

    1810c

--- CF1810C.go
+++ CF1810C.go
@@ -12,21 +12,40 @@ var (
 	in  = bufio.NewReader(os.Stdin)
 )
 
+func min(a int , b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func main() {
 	defer out.Flush()
-	var T, n , c , d int
+	var T, n, c, d int
 	fmt.Fscan(in, &T)
 
 	for i := 0; i < T; i++ {
-		fmt.Fscan(in, &n , &c , &d)
-		var a , b []int
-		var res int
-		for i := 0 ; i < n ; i ++ {
-			fmt.Fscan(in,&a[i])
+		fmt.Fscan(in, &n, &c, &d)
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &a[i])
 		}
 		sort.Slice(a, func(i, j int) bool {
 			return a[i] < a[j]
 		})
-		
+		var per = 0
+		var res = c * n + d
+		var add = 0
+		for i := 0 ; i < n ; i ++ {
+			now := a[i]
+			if now > per {
+				add += d * (now - per - 1)
+			}else if now == per {
+				add += c;
+			}
+			res = min(res, add + (n - i - 1) * c);
+			per = now;
+		} 
+		fmt.Println(res)
 	}
 }
